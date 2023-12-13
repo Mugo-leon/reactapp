2c51155633f3485191d5735e6d120691ec32f5e8
Mugo-leon <144475601+Mugo-leon@users.noreply.github.com>
2023-12-13 22:05:31 +0300

    Revert "Added header functionality"

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Header from "./Header";
-
-
-const App = () => {
-  
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('http://localhost:3000/Immunizations');
-  //       const data = await response.json();
-  //       setImmunizationData(data);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-
+function App() {
   return (
-    <div >
-  
-
-          <Header isLoggedIn={true} />
-      
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
