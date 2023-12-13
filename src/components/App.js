f362c2164b3147608a272c459934b90d6210a3eb
Leon <leon.muriuki@student.moringaschool.com>
2023-12-14 00:42:43 +0300

    added childageselector functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from 'react';
 import ChildAgeSelector from './ChildAgeSelector';
 
 const App = () => {
 
+
   return (
     <div>
-        <>
           <ChildAgeSelector onSelect={(age) => (age)} />
-        </>
+    </div>
+  );
+};
 
 export default App;
