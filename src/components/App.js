4543a029bd1e5c6571439f049ef61b478254469a
Janemwaura <mwaura461@gmail.com>
2023-12-13 23:16:13 +0300

    added darkmode

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import DarkModeToggle from './DarkModeToggle';
+
+
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleDarkModeChange = (newMode) => {
+    setIsDarkMode(newMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isDarkMode ? 'dark-mode' : ''}>
+      <DarkModeToggle isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeChange} />
+      
+        
+      
     </div>
   );
-}
+};
 
 export default App;
